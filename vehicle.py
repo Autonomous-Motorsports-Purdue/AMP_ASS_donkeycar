28d86957dda7b1d68309b12c622ca6cb01cf2bbc
colinboumenot <colin.boumenot@yahoo.com>
2024-10-12 10:04:31 -0400

    #6 Driver class done

--- vehicle.py
+++ vehicle.py
@@ -4,10 +4,7 @@ from donkeycar.vehicle import Vehicle as BaseVehicle
 logger = logging.getLogger(__name__)
 
 class Vehicle(BaseVehicle):
-
-    ## Removed option for threading
-    def add(self, part, inputs = [], outputs = [], run_condition = None):
-        super().add(part, inputs, outputs, False, run_condition)
+    pass
     
     
 
