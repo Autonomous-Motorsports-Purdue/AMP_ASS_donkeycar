f114931b7eb69cb27787a6d86dfae2b2a02a5140
Michael Crothers <58677365+Nrxszv0@users.noreply.github.com>
2024-10-26 14:21:36 -0400

    added new logger

--- parts/logger.py
+++ parts/logger.py
@@ -2,40 +2,42 @@ import datetime
 import cv2
 import os
 import numpy as np
+import csv
 class Logger():
     def __init__(self):
-        self.image_directory = "data/images/" + datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + "/"
-        self.point_directory = "data/points/" + datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + "/"
+        start_time = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+        self.image_directory = "data/images/" + start_time
+        self.segmented_directory = "data/segmented_images/" +start_time
         # self.depth_directory = "data/depth/"  + datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + "/"
         if not os.path.exists(self.image_directory):
             os.makedirs(self.image_directory)
-        if not os.path.exists(self.point_directory):
-            os.makedirs(self.point_directory)
+        if not os.path.exists(self.segmented_directory):
+            os.makedirs(self.segmented_directory)
+        self.info_csv = "data/information/" + start_time + ".csv"
         # if not os.path.exists(self.depth_directory):
             # os.makedirs(self.depth_directory)
+        # writing to csv file
+        with open(self.info_csv, 'w') as self.csvfile:
+            # creating a csv writer object
+            self.csvwriter = csv.writer(self.csvfile)
 
-    def run(self, image, depth, points):
-        if image is not None and points is not None and points.size != 0:
-            image_file = self.image_directory + "/" + datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S.%f') + ".jpg"
-            point_file = self.point_directory + "/" + datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S.%f') + ".t"
+            fields = ['timestamp', 'image', 'segmented', 'centroid', 'steering', 'throttle']
+            # writing the fields
+            self.csvwriter.writerow(fields)
+        
+    def run(self, image, segmentedImage, centroid, steering, throttle):
+        if image is not None and segmentedImage is not None:
+            timestamp = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S.%f')
+            image_file = self.image_directory +"/"+ timestamp + ".jpg"
+            segmented_file = self.point_directory + "/" + timestamp + ".jpg"            
 
-            np.savetxt(point_file, points)
-            # depth_file = self.depth_directory + "/" + datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S.%f') + ".jpg"
-            print(f"Attempting to save file: {image_file}")
-            # Save the image and check if it was successful
-            success = cv2.imwrite(image_file, image)
-            if success:
-                print(f"File saved successfully: {image_file}")
-            else:
-                print("Failed to save the file. Check if the image is valid.")
+            # Save the images if written is successful
+            success_image = cv2.imwrite(image_file, image)
+            success_segmented = cv2.imwrite(segmented_file, segmentedImage)
+            if success_image and success_segmented:
+                rows = [timestamp, image_file, segmented_file, centroid, steering, throttle]    
+                self.csvwriter.writerow(rows)
+            
+            
+        
 
-            # Save the depth image and check if it was successful
-            # success = cv2.imwrite(depth_file, depth)
-            # if success:
-                # print(f"File saved successfully: {depth_file}")
-            # else:
-                # print("Failed to save the file. Check if the depth image is valid.")
-
-            # cv2.imshow("image", image)
-            # cv2.imshow("depth", depth)
-            # cv2.waitKey(1)
